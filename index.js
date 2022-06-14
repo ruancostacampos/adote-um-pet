c08e2b3c4dda42b0a674411ba4b4124c36b55e12
cyberpunk <ruancosta9797@gmail.com>
2022-06-14 20:24:28 -0300

    Configurando CORS

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const routes = require('./controllers/routeController')
 // ======== Allow CORS ========== 
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.FRONT_URL);
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     app.use(cors());
     next();
   });

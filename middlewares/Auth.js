dd83d880d0521183081968dcddc19ecbaa7dc453
cyberpunk <ruancosta9797@gmail.com>
2022-06-14 17:41:06 -0300

    Configurando autenticação básica

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,12 +1,12 @@
-const auth = (req, res, next) => {
-  var ip = req.headers['x-forwarded-for'] ||
+const auth = async (req, res, next) => {
+  var ip = req.headers['x-forwarded-for'][0] ||
   req.socket.remoteAddress ||
   null;
 
   if(ip === process.env.ADMIN_IP){
     next()
   }else{
-    res.send(401).json({message: "Você não tem autorização para usar esse recurso."})
+    res.send(200).json({message: "Você não tem autorização para usar esse recurso."})
   }
 }
 
